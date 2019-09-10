d2b255f971a2ce5d1937b5dcc135cd7e53c0f838
liang.zhao <liang.zhao@unity3d.com>
2019-09-10 18:01:00 +0800

    [LZ] add cube presentation.

---
+++ prj19.3/Assets/Scripts/Client/Systems/PlayerPresentation.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Collections;
+
+[DisableAutoCreation]
+[UpdateInGroup(typeof(ClientPresentationSystemGroup))]
+public class PlayerPresentation : ComponentSystem
+{
+    EntityQuery createCubeGoQuery;
+    EntityQuery updateCubeGoQuery;
+
+    protected override void OnCreate()
+    {
+        createCubeGoQuery = GetEntityQuery(
+            ComponentType.ReadOnly<RepCubeComponentData>(),
+            ComponentType.Exclude<RepCubeGoCreated>());
+        updateCubeGoQuery = GetEntityQuery(ComponentType.ReadOnly<RepCubeGoCreated>());
+    }
+
+    protected override void OnUpdate()
+    {
+        var cubeEntities = createCubeGoQuery.ToEntityArray(Allocator.TempJob);
+        for (int i = 0; i < cubeEntities.Length; ++i)
+        {
+            var cubeEnt = cubeEntities[i];
+            ReplicatedPrefabMgr.LoadPrefabIntoEntity("assets_prefabs_cube", World, cubeEnt);
+            EntityManager.AddComponentData(cubeEnt, default(RepCubeGoCreated));
+        }
+        cubeEntities.Dispose();
+
+        var goEntities = updateCubeGoQuery.ToEntityArray(Allocator.TempJob);
+        for (int i = 0; i < goEntities.Length; ++i)
+        {
+            var goEnt = goEntities[i];
+
+            var transform = EntityManager.GetComponentObject<Transform>(goEnt);
+            var cubeData = EntityManager.GetComponentData<RepCubeComponentData>(goEnt);
+            transform.position = cubeData.position;
+        }
+        goEntities.Dispose();
+    }
+}
+
+public struct RepCubeGoCreated : IComponentData { }
